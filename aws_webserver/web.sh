b087389960b567da50e3ab123db126c3c93b0598
sagarhere4u <55478536+sagarhere4u@users.noreply.github.com>
2023-12-01 11:58:39 +0530

    Update web.sh

--- aws_webserver/web.sh
+++ aws_webserver/web.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt-get update -y
 sudo apt-get install apache2 -y
-echo '<!doctype html><html><body><h1>Hello, Welcome to IaC with DevOps World. Happy Dhanteras. Thank You!</h1></body></html>' | sudo tee /var/www/html/index.html
+echo '<!doctype html><html><body><h1>Hello, Welcome to IaC with DevOps World. Thank You!</h1></body></html>' | sudo tee /var/www/html/index.html
 sudo systemctl restart apache2
 sudo systemctl enable apache2
