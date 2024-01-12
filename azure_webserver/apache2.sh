83d7921bf80ee4809f706b8f301a91baf59e3f4e
Sagar Mehta <55478536+sagarhere4u@users.noreply.github.com>
2024-01-12 10:53:45 +0530

    Update apache2.sh

--- azure_webserver/apache2.sh
+++ azure_webserver/apache2.sh
@@ -3,7 +3,7 @@
 sudo apt-get update && sudo apt-get -y install apache2
 
 
-echo '<!doctype html><html><body><h1>Hello, Merry Christmas Everyone!</h1></body></html>' | sudo tee /var/www/html/index.html
+echo '<!doctype html><html><body><h1>Hello, Happy New Year Everyone!</h1></body></html>' | sudo tee /var/www/html/index.html
 
 sudo systemctl restart apache2
 sudo systemctl enable apache2
