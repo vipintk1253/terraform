6c18bc9926afbc532932754d254d3e39cf25d2a1
Sagar Mehta <55478536+sagarhere4u@users.noreply.github.com>
2024-01-26 19:06:35 +0530

    Add files via upload

---
+++ docker_webserver/main.tf
@@ -0,0 +1,34 @@
+terraform {
+  required_providers {
+    docker = {
+      source = "kreuzwerker/docker"
+      version = "3.0.2"
+    }
+  }
+}
+
+provider "docker" {
+  host = "unix:///var/run/docker.sock"
+}
+
+resource "docker_image" "apache" {
+  name = "smehta26/apache:alpine"
+  build {
+    context = "."
+  }
+}
+
+resource "docker_container" "apache" {
+  name  = "apache"
+  image = docker_image.apache.image_id
+
+  ports {
+    internal = "80"
+    external = "8100"
+  }
+
+  volumes {
+    container_path = "/var/www/html/index.html"
+    host_path      = "index.html"
+  }
+}
