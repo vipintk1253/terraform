a9b8455393c7e0484f18cf0ab5003e18507e096b
Sagar Mehta <55478536+sagarhere4u@users.noreply.github.com>
2024-02-16 15:41:11 +0530

    Update apache2.sh

--- azure_webserver/apache2.sh
+++ azure_webserver/apache2.sh
@@ -3,7 +3,7 @@
 sudo apt-get update && sudo apt-get -y install apache2
 
 
-echo '<!doctype html><html><body><h1>Hello, Welcome to DevOps World for IaC. 16th Feb'24!</h1></body></html>' | sudo tee /var/www/html/index.html
+echo '<!doctype html><html><body><h1>Hello, Welcome to DevOps World for IaC. 16th Feb2024!</h1></body></html>' | sudo tee /var/www/html/index.html
 
 sudo systemctl restart apache2
 sudo systemctl enable apache2
