f1d02c975d2226073e9da628867254d56892e994
Sagar Mehta <55478536+sagarhere4u@users.noreply.github.com>
2024-02-16 11:57:36 +0530

    Update main.tf

--- azure_webserver/main.tf
+++ azure_webserver/main.tf
@@ -74,7 +74,7 @@ resource "azurerm_network_interface" "main" {
 }
 
 resource "azurerm_linux_virtual_machine" "main" {
-  name                            = "${trimspace(data.template_file.prefix.rendered)}-newindia-vm"
+  name                            = "${trimspace(data.template_file.prefix.rendered)}-vm"
   resource_group_name             = azurerm_resource_group.main.name
   location                        = azurerm_resource_group.main.location
   size                            = "Standard_F1"
