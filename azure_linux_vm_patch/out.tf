d074bb22f51f2ae5d4f48a333af8eda8c46b5e2d
Sagar Mehta <55478536+sagarhere4u@users.noreply.github.com>
2024-03-04 21:24:59 +0530

    Update out.tf

--- azure_linux_vm_patch/out.tf
+++ azure_linux_vm_patch/out.tf
@@ -1,28 +1,3 @@
-output "resource_group_name" {
-  value = azurerm_resource_group.example.name
+output "MyPublicIP" {
+  value = azurerm_linux_virtual_machine.main.public_ip_address
 }
-
-output "kubernetes_cluster_name" {
-  value = azurerm_kubernetes_cluster.example.name
-}
-
-output "host" {
-  value = azurerm_kubernetes_cluster.example.kube_config.0.host
-  sensitive = true
-}
-
-output "client_key" {
-  value = azurerm_kubernetes_cluster.example.kube_config.0.client_key
-  sensitive = true
-}
-
-output "client_certificate" {
-  value     = azurerm_kubernetes_cluster.example.kube_config.0.client_certificate
-  sensitive = true
-}
-
-output "kube_config" {
-  value = azurerm_kubernetes_cluster.example.kube_config_raw
-  sensitive = true
-}
-
