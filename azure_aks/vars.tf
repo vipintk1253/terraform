17d13271661bcbb04f02c08cc04f0da09a8c8185
Sagar Mehta <55478536+sagarhere4u@users.noreply.github.com>
2024-03-04 21:17:16 +0530

    Update vars.tf

--- azure_aks/vars.tf
+++ azure_aks/vars.tf
@@ -1,23 +1,3 @@
-variable "client_certificate_path" {
-  default = "/root/sagarcert/sagarcert.pfx"
-}
-
-variable "subscription_id" {
-  default = "c1fb0dc4-1896-49c0-8c25-a03bb9a17055"
-}
-
-variable "client_id" {
-  default = "d7af4115-871c-4ccf-ab45-3b228961c4d1"
-}
-
-variable "tenant_id" {
-  default = "df8af829-df8a-41fe-8679-3e0a616165c5"
-}
-
-variable "prefix" {
-  default = "terraform-aks"
-}
-
 variable "location" {
   default = "Central India"
 }
