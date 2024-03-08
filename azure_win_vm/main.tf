9ae4a8b0f4c066ae9fb7d73c62fec1ea814b8a69
Sagar Mehta <55478536+sagarhere4u@users.noreply.github.com>
2024-03-08 15:54:45 +0530

    Update main.tf

--- azure_win_vm/main.tf
+++ azure_win_vm/main.tf
@@ -51,7 +51,7 @@ resource "azurerm_network_security_group" "example" {
     access                     = "Allow"
     protocol                   = "Tcp"
     source_port_range          = "*"
-    destination_port_range     = "6515-6000"
+    destination_port_range     = "0-65535"
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
