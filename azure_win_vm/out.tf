26cd0ce174e39b80861ce8f3618f88101829b3f5
Sagar Mehta <55478536+sagarhere4u@users.noreply.github.com>
2024-03-08 14:33:27 +0530

    Update out.tf

--- azure_win_vm/out.tf
+++ azure_win_vm/out.tf
@@ -1,16 +1,16 @@
 output "PrivateIP" {
-  value = azurerm_windows_virtual_machine.example.private_ip_address
+  value = azurerm_virtual_machine.example.private_ip_address
 }
 
 output "PublicIP" {
-  value = azurerm_windows_virtual_machine.example.public_ip_address
+  value = azurerm_virtual_machine.example.public_ip_address
 }
 
 output "UserName" {
-  value = azurerm_windows_virtual_machine.example.admin_username
+  value = azurerm_virtual_machine.example.admin_username
 }
 
 output "Password" {
-  value = azurerm_windows_virtual_machine.example.admin_password
+  value = azurerm_virtual_machine.example.admin_password
   sensitive = true
 }
